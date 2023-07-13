17fffbfb7cbe7a73dc01651803d7c65f78ac647e
Vittorio Pippi <219424@studenti.unimore.it>
2023-07-13 12:39:08 +0200

    Downloading the next block in background

--- font_square.py
+++ font_square.py
@@ -1,19 +1,49 @@
 import io
 import time
 import json
-from typing import Iterator
 import torch
 import requests
-import warnings
+import threading
 import numpy as np
 from pathlib import Path
 from torchvision.transforms import ToTensor
 from torch.utils.data import IterableDataset, DataLoader
 
 
+class BlockLoaderThread(threading.Thread):
+    def __init__(self, url, destination=None):
+        super().__init__()
+        self.url = url
+        self.destination = destination
+        self.images = None
+        self.images_wide = None
+        self.images_idx = None
+
+
+    def run(self):
+        if self.destination is not None and self.destination.exists():
+            data = np.load(self.destination)
+        else:
+            response = requests.get(self.url)
+            response.raise_for_status()
+            content = response.content
+            if self.destination is not None:
+                self.destination = Path(self.destination)
+                self.destination.parent.mkdir(exist_ok=True, parents=True)
+                with open(self.destination, "wb") as f:
+                    f.write(content)
+            data = np.load(io.BytesIO(content))
+
+        self.images = data['images']
+        self.images_wide = data['width']
+        self.images_wide = np.cumsum(self.images_wide)
+        self.images_wide = np.concatenate([np.array([0,], dtype=self.images_wide.dtype), self.images_wide])
+        self.images_idx = data['idx']
+
+
 class Font2(IterableDataset):
-    def __init__(self, path, transform=ToTensor(), nameset='train', fonts_ids=None,
-                 words_ids=None, store_on_disk=False, auto_download=False, shuffle=False):
+    def __init__(self, path, transform=ToTensor(), nameset='train', fonts_ids=None, words_ids=None,
+                 store_on_disk=False, auto_download=False, shuffle=False, load_next_block=True):
         super(Font2).__init__()
         self.blocks_base_url = 'https://github.com/aimagelab/font_square/releases/download/Dataset/'
 
@@ -24,6 +54,8 @@ class Font2(IterableDataset):
         self.blocks_path = self.path / 'blocks'
         self.idx = None
         self.shuffle = shuffle
+        self.block_loader = None
+        self.load_next_block = load_next_block
         
         self.num_workers = None
         self.worker_id = None
@@ -45,32 +77,32 @@ class Font2(IterableDataset):
         self.blocks, self.sizes = zip(*self.blocks)
 
     
-    def load_block(self, idx=0):
+    def load_block(self, idx=0, load_next=False):
         assert self.worker_blocks is not None, 'You must call assign_blocks before load_block'
         block_to_load = self.blocks_path / self.worker_blocks[idx]
-        if not block_to_load.exists() and self.auto_download:
-            data = self.download(block_to_load)
-            data = np.load(io.BytesIO(data.content))
-        elif block_to_load.exists():
-            data = np.load(block_to_load)
-        else:
-            raise FileNotFoundError(f'Block {block_to_load} not found. Download it manually from the repo or set auto_download=True.')
-
-        self.images = data['images']
-        self.images_wide = data['width']
-        self.images_wide = np.cumsum(self.images_wide)
-        self.images_wide = np.concatenate([np.array([0,], dtype=self.images_wide.dtype), self.images_wide])
-        self.images_idx = data['idx']
+        
+        if self.block_loader is None:
+            self.block_loader = self.make_loader(block_to_load)
+            self.block_loader.start()
+        self.block_loader.join()
+        
+        self.images = self.block_loader.images
+        self.images_wide = self.block_loader.images_wide
+        self.images_idx = self.block_loader.images_idx
+        self.block_loader = None
+        
+        if load_next and idx + 1 < len(self.worker_blocks):
+            block_to_load = self.blocks_path / self.worker_blocks[idx + 1]
+            self.block_loader = self.make_loader(block_to_load)
+            self.block_loader.start()
     
-    def download(self, block):
+    
+    def make_loader(self, block):
         url = self.blocks_base_url + block.name
-        response = requests.get(url)
-        response.raise_for_status()
-        block.parent.mkdir(exist_ok=True, parents=True)
-        if self.store_on_disk:
-            with open(block, "wb") as f:
-                f.write(response.content)
-        return response
+        block = block if self.store_on_disk else None
+        downloader = BlockLoaderThread(url, block)
+        return downloader
+    
     
     def collate_fn(self, batch):
         imgs, widths, font_ids, words = zip(*batch)
@@ -80,6 +112,7 @@ class Font2(IterableDataset):
         widths = torch.Tensor(widths)
         font_ids = torch.Tensor(font_ids)
         return imgs, widths, font_ids, words
+    
     
     def __getitem__(self, idx):
         width_start = self.images_wide[idx]
@@ -96,6 +129,7 @@ class Font2(IterableDataset):
 
         return img, width, font_id, word
     
+    
     def assign_blocks(self):
         worker_info = torch.utils.data.get_worker_info()
         self.num_workers = 1 if worker_info is None else worker_info.num_workers
@@ -107,13 +141,14 @@ class Font2(IterableDataset):
             blocks_sizes = list(zip(self.worker_blocks, self.worker_sizes))
             np.random.shuffle(blocks_sizes)
             self.worker_blocks, self.worker_sizes = zip(*blocks_sizes)
-    
+       
+            
     def __iter__(self):
         self.assign_blocks()
         
         if len(self.worker_blocks) > 0:
             self.idx = 0
-            self.load_block(0)
+            self.load_block(0, load_next=self.load_next_block)
         
         while len(self.worker_blocks) > 0:
             if self.idx >= len(self.images_idx):
@@ -123,7 +158,7 @@ class Font2(IterableDataset):
                 
                 if len(self.worker_blocks) == 0:
                     break
-                self.load_block(0)
+                self.load_block(0, load_next=self.load_next_block)
                 
             img_idx = self.images_idx[self.idx]
             font_id = img_idx // len(self.words)
@@ -133,9 +168,10 @@ class Font2(IterableDataset):
                 yield self[self.idx]
             self.idx += 1
 
+
 if __name__ == '__main__':
     db = Font2('.', store_on_disk=True, auto_download=True, nameset='train')
-    loader = DataLoader(db, batch_size=32, num_workers=2, collate_fn=db.collate_fn)
+    loader = DataLoader(db, batch_size=32, num_workers=0, collate_fn=db.collate_fn)
     start = time.perf_counter()
     counter = 0 
     try:
